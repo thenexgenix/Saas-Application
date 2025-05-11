24f2879e2c831af23b2843766db6e102fe71a7bc
Asaduzzaman04 <asadchy152@gmail.com>
2025-05-11 19:26:19 +0600

    backend setup

---
+++ backend/src/config/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+const app = express();
+
+//default middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use("/images", express.static("upload")); // for file upload
+
+//your routes start from here
+
+//export the express app
+export default app;
